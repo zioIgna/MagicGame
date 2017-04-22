0a5d7ee440101ad408e0dacf5be85a5eafa678b8
ignaz <ignaz@LAPTOP-U79M7UNK>
2017-04-22 12:36:53 +0200

    Modifiche sulle classi Affliction, AfflictionDecorator, Decorator

---
+++ src/cardgame/cards/BoilingEarth.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame.cards;
+
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.Creature;
+import cardgame.Effect;
+import cardgame.Player;
+import java.util.List;
+
+/**
+ *
+ * @author ignaz
+ */
+public class BoilingEarth implements Card {
+    private class BoilingEarthEffect extends AbstractCardEffect {
+        public BoilingEarthEffect(Player p, Card c) { super(p,c); }
+        @Override
+        public void resolve() {
+            List<Creature> attackers = owner.getCreatures();
+            for (Creature e : attackers) {
+                e.inflictDamage(1);
+            }
+            List<Creature> defenders = CardGame.
+        }
+    }
+
+    @Override
+    public Effect getEffect(Player owner) { 
+        return new BoilingEarthEffect(owner, this); 
+    }
+    
+    @Override
+    public String name() { return "Boiling Earth"; }
+    @Override
+    public String type() { return "Sorcery"; }
+    @Override
+    public String ruleText() { return name() + " deals 1 damage to each creature"; }
+    @Override
+    public String toString() { return name() + " (" + type() + ") [" + ruleText() +"]";}
+    @Override
+    public boolean isInstant() { return false; }
+}
